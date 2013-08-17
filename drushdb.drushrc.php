b5c0889b24ea7f2dbdd264b576e54e6e39e46d60
Paul Byrne <paul@pfaocle.co.uk>
2013-08-17 22:36:04 +0100

    Add commented out config for Drush verbose mode.

--- drushdb.drushrc.php
+++ drushdb.drushrc.php
@@ -5,6 +5,9 @@
  * Additional Drush configuration used when running commands via DrushDb/Codeception.
  */
 
+// Uncomment this for increased Drush verbosity.
+//$options['verbose'] = 1;
+
 $options['structure_tables'] = array(
   'common' => 'cache',
 );
